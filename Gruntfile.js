77d3fba1439f8c0e7f444d8222e176fad0fbe485
jastylr <jtaylor@faultlinemedia.com>
2015-07-02 12:59:00 -0700

    Fix image paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -204,8 +204,8 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          // '<%= yeoman.dist %>/styles/{,*/}*.css',
+          // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
