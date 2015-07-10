2b67c81eef41c1978ab0b85738450e310be223ba
jastylr <jtaylor@faultlinemedia.com>
2015-07-09 22:10:59 -0700

    Fix CSS z-index issues on containers

--- app/scripts/controllers/navController.js
+++ app/scripts/controllers/navController.js
@@ -1,7 +1,18 @@
 'use strict';
 
 app.controller('NavController', function($scope, $location) {
+  // Mobile menu is initally collapsed
+  $scope.isCollapsed = true;
+  
+  // Detect when the route changes (ie. a menu link was clicked)
+  $scope.$on('$routeChangeSuccess', function () {
+      // Set isCollapsed to true. The ng-class directive on the navbar
+      // will bet set to "collapse" whenever the route changes 
+      $scope.isCollapsed = true;
+  });
 
+  // Used to determine the active link in the navbar so that
+  // a class can be set to style it
   $scope.isActive = function (viewLocation) {
     var active = (viewLocation === $location.path());
     return active;
